c17f5a80f174537a7a16ac8f924c4ce53fa3c551
Your Name <you@example.com>
2018-06-17 14:46:22 +0800

    bug fix

--- src/test/java/JarTest.java
+++ src/test/java/JarTest.java
@@ -4,11 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 /**
  * Created by WZL on 2018/6/14.
@@ -29,8 +25,10 @@ public class JarTest {
         URL url = loader.getResource(packagePath);
         if (File.separator.equals("\\")){
             Assert.assertTrue(url.getPath().startsWith("/"));
-        } else {
+        } else if (url.getProtocol().equals("jar")){
             Assert.assertTrue(url.getPath().startsWith("file:"));
+        }else {
+
         }
 
     }
